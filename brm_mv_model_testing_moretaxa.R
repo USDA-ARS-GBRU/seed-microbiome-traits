6c7fd4635e0815c78183c31ced2b59a577d7837b
Quentin Read <quentin.read@usda.gov>
2024-07-25 11:51:32 -0400

    Simulate MV data with an actual signal for the model to pick up on!

--- brm_mv_model_testing_moretaxa.R
+++ brm_mv_model_testing_moretaxa.R
@@ -19,22 +19,22 @@ sigma_maternal <- cov(X_maternal)
 # Coefficients indicating which taxa predict the outcome.
 # We will not include any interaction effect.
 # Only include a few taxa with a nonzero effect.
-beta <- c(10, 10, -10, rep(0, n_taxa-3))
+beta <- c(20, 10, -10, rep(0, n_taxa-3))
 
-y_maternal <- 10 + X_maternal %*% beta + rnorm(n_mothers, 0, 1)
+y_maternal <- 0 + X_maternal %*% beta + rnorm(n_mothers, 0, 1)
 
-# To get offspring microbiome, take mvnorm draws from the mean vector for each mother (rows of X_maternal)
-# using the same covariance matrix every time.
-
+# To get offspring microbiome, take multivariate normal draws from the mean vector for each mother (rows of X_maternal)
 X_offspring <- apply(X_maternal, 1, function(Xi) rmvnorm(offspring_per_mother, mean = Xi, sigma = sigma_maternal), simplify = FALSE)
-y_offspring <- rnorm(offspring_per_mother * n_mothers, mean = rep(y_maternal, offspring_per_mother), sd = 1)
+
+# Use regression coefficients (beta) to get value for offspring trait, plus noise
+y_offspring <- lapply(X_offspring, function(Xoi) Xoi %*% beta + rnorm(offspring_per_mother, 0, 1))
 
 # Combine together
 dt <- data.frame(
   maternal_id = factor(rep(1:n_mothers, each = offspring_per_mother)),
   offspring_id = 1:offspring_per_mother,
   do.call(rbind, X_offspring),
-  y = y_offspring
+  y = do.call(c, y_offspring)
 )
 
 # Within each mother, set half of the values to be missing for x, and the other half for y.
