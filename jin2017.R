3e9df67bbcd86be434085e390a0db362aeb50de4
Quentin Read <quentin.read@usda.gov>
2025-01-30 16:24:18 -0500

    Write up models for Jin 2017

--- jin2017.R
+++ jin2017.R
@@ -48,6 +48,8 @@ fwrite(traits_otu, 'project/data/jin2017/traits_otupca.csv')
 
 # Test relationship with traits -------------------------------------------
 
+traits_otu <- fread('project/data/jin2017/traits_otupca.csv')
+
 # Determine which traits have the highest correlation with the PCA axes
 cormat <- cor(traits_otu[, mget(c(trait_names[-1], paste0('PC', 1:10)))], use = 'pairwise.complete.obs')[trait_names[-1], paste0('PC', 1:10)]
 sort(apply(abs(cormat), 1, mean))
@@ -62,3 +64,86 @@ set.seed(333)
 glmnet_test <- cv.glmnet(x = as.matrix(traits_otu[, mget(paste0('PC', 1:10))]), y = log(traits_otu$grain_weight), alpha = 1, nfolds = 10, type.measure = 'mse',
           lambda = seq(0.001, 0.1, by = 0.001))
 
+
+# Analysis grouped by cultivar --------------------------------------------
+
+# Use only data with >1 observation per cultivar
+multiple_obs_cultivars <- unique(traits_otu$cultivar[duplicated(traits_otu$cultivar)])
+
+traits_otu_multipleobs <- traits_otu[cultivar %in% multiple_obs_cultivars]
+
+library(lme4)
+lmer_test <- lmer(log(grain_weight) ~ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10 + (1|cultivar), data=traits_otu_multipleobs)
+
+
+# BRM model with no missing values ----------------------------------------
+
+# Use similar horseshoe priors
+library(brms)
+library(rstan)
+options(brms.backend = 'cmdstanr', brms.file_refit = 'on_change', mc.cores = 4)
+
+n_PCs <- 10
+
+# Construct priors programmatically because we have one for each taxon.
+sd_X_priors <- lapply(1:n_PCs, function(i) prior_string('gamma(1, 1)', class = 'sd', resp = paste0('PC', i)))
+sd_X_priors <- do.call(c, sd_X_priors)
+sigma_X_priors <- lapply(1:n_PCs, function(i) prior_string('gamma(1, 1)', class = 'sigma', resp = paste0('PC', i)))
+sigma_X_priors <- do.call(c, sigma_X_priors)
+
+# Also construct formula programmatically.
+X_formula <- paste0('mvbind(', paste(paste0('PC',1:n_PCs), collapse = ','), ') ~ 0 + (1||cultivar)')
+y_formula <- paste0('grain_weight ~ ', paste(paste0('PC',1:n_PCs), collapse = '+'), ' + (1||cultivar)')
+
+fit_grainweight_nomiss <- brm(
+  bf(mvbind(PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10) ~ 0 + (1||cultivar)) + bf(grain_weight ~ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10 + (1||cultivar)) + set_rescor(FALSE),
+  prior = c(
+    sd_X_priors,
+    sigma_X_priors,
+    prior(gamma(1, 1), class = sd, resp = grainweight),
+    prior(gamma(1, 1), class = sigma, resp = grainweight),
+    prior(horseshoe(df = 1, df_global = 1, scale_slab = 20, df_slab = 10, par_ratio = 1), class = b, resp = grainweight) 
+  ),
+  data = traits_otu_multipleobs,
+  chains = 4, iter = 7500, warmup = 5000,
+  init = 0, seed = 1004,
+  control = list(adapt_delta = 0.95),
+  file = 'project/fits/brm_grainweight_nomiss'
+)
+
+
+# BRM model with missing values -------------------------------------------
+
+# Artificially generate the missing data
+# 50% of the individuals within each cultivar will have missing trait data, and 50% will have missing microbiome data
+set.seed(444)
+
+# Function to randomly sample half
+samplehalf <- function(n) sample(rep(1:2, times=ceiling(n/2)))[1:n]
+
+traits_otu_withmiss <- copy(traits_otu_multipleobs)
+traits_otu_withmiss[, grp := samplehalf(.N) , by = .(cultivar)]
+
+traits_otu_withmiss[grp == 1, grain_weight := NA]
+PCcols <- paste0('PC', 1:n_PCs)
+traits_otu_withmiss[ , (PCcols) := lapply(.SD, function(x) fifelse(grp == 2, NA_real_, x)), .SDcols = PCcols ]
+
+# Construct formulas programmatically.
+Xmiss_formula <- paste0('mvbind(', paste(PCcols, collapse = ','), ') | mi() ~ 0 + (1||cultivar)')
+ymiss_formula <- paste0('grain_weight | mi() ~ ', paste(paste0('mi(PC', 1:n_PCs, ')'), collapse = '+'), ' + (1||cultivar)')
+
+fit_grainweight_miss <- brm(
+  bf(mvbind(PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10) | mi() ~ 0 + (1||cultivar)) + bf(grain_weight | mi() ~ mi(PC1)+mi(PC2)+mi(PC3)+mi(PC4)+mi(PC5)+mi(PC6)+mi(PC7)+mi(PC8)+mi(PC9)+mi(PC10) + (1||cultivar)) + set_rescor(FALSE),
+  prior = c(
+    sd_X_priors,
+    sigma_X_priors,
+    prior(gamma(1, 1), class = sd, resp = grainweight),
+    prior(gamma(1, 1), class = sigma, resp = grainweight),
+    prior(horseshoe(df = 1, df_global = 1, scale_slab = 20, df_slab = 10, par_ratio = 1), class = b, resp = grainweight) 
+  ),
+  data = traits_otu_withmiss,
+  chains = 4, iter = 7500, warmup = 5000,
+  init = 0, seed = 1104,
+  control = list(adapt_delta = 0.95),
+  file = 'project/fits/brm_grainweight_miss'
+)
